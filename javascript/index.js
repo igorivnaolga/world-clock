309d315e268b3f35e71e4c3eff591dacbeea24a6
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2022-11-03 13:45:26 +1300

    Added js function updateTime

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,31 @@
+function updateTime() {
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
 
+  tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = moment()
+    .tz("Asia/Tokyo")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+
+  sydneyDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
