28215b8407797f99978554c428227867fea6affb
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2022-11-04 15:25:56 +1300

    Added functionality for select

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 function updateTime() {
   let tokyoElement = document.querySelector("#tokyo");
+
   let tokyoDateElement = tokyoElement.querySelector(".date");
   let tokyoTimeElement = tokyoElement.querySelector(".time");
 
@@ -27,5 +28,25 @@ function updateTime() {
     .format("h:mm:ss [<small>]A[</small>]");
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
